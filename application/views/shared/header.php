054839e4d5f3188ac777b16222c0b0f04cf13da0
Anthony Tumbiolo <anthony.tumbiolo@gmail.com>
2012-06-22 10:03:49 -0400

    added google maps to listings

--- application/views/shared/header.php
+++ application/views/shared/header.php
@@ -5,14 +5,22 @@
     <title><?php echo $title ?></title>
     <?php echo css_asset('bootstrap.min.css', 'bootstrap'); ?>
     <?php echo css_asset('main.css'); ?>
+    <?php echo css_asset('jquery.fancybox.css'); ?>
     <link rel="stylesheet" type="text/css" href="http://joshuakehn.com/static/justfonts.css" />
 
     <!-- Load jQuery -->
     <script type="text/javascript"
  src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 
-    <!-- Bootstrap js -->
+    <!-- Bootstrap.js -->
     <script type="text/javascript" src="<?php echo js_asset_url('bootstrap.min.js', 'bootstrap'); ?>"></script>
+
+    <!-- Fancybox.js -->
+    <script type="text/javascript" src="<?php echo js_asset_url('jquery.fancybox.js'); ?>"></script>
+
+    <!-- Easing.js -->
+    <script type="text/javascript" src="<?php echo js_asset_url('jquery.easing.js'); ?>"></script>
+
 </head>
 
 <body>
--- application/views/smartass_controller/spaces.php
+++ application/views/smartass_controller/spaces.php
@@ -10,7 +10,20 @@
         <div class="span10 offset1 available-space">
             <p class="listing-title">2 desks at 44 Wall St 13th Floor</p>
             <p class="listing-description">We are a group of 3, 2 software developers and 1 stock broker looking to move into a bigger corner office on the 13th floor. We need to fill 2 desks.</p>
-            <p class="address"><em>Address:</em> 44 Wall St. (click for map)</p>
+            <?php
+            $address = '44 Wall St.';
+            // manipulate address to only get what needed for google map api
+            $google_address = explode('(', $address);
+            $google_address = trim($google_address[0]);
+            $google_address = str_replace(' ', '+', $google_address);
+            ?>
+            <p class="address"><em>Address:</em> <?php echo $address ?> - <a id="googlemaps" href="#map">Google Map</a>
+                <div style="display:none">
+                    <div id="map">
+                        <img src="https://maps.googleapis.com/maps/api/staticmap?center=<?php echo $google_address; ?>,New+York,NY&zoom=15&size=500x400&maptype=roadmap&markers=color:red%7Clabel:A%7C<?php echo $google_address?>,New+York,NY&sensor=false" />
+                    </div>
+                </div>
+            </p>
             <p class="desks-available"><em>Desks Available:</em> 2</p>
             <p class="desk-price"><em>Price per Desk:</em> $315</p>
             <p class="lease-duration"><em>Lease Duration:</em> 6 months</p>
@@ -36,5 +49,18 @@
 </div>
 
 <?php $this->load->view('shared/navbar'); ?>
+
+<script type="text/javascript">
+
+jQuery(function() {
+    $("a#googlemaps").fancybox({
+        'hideOnContentClick': false,
+        'autoDimensions': false,
+        'height': 403,
+        'width' : 500
+    });
+})
+
+</script>
 
 <?php $this->load->view('shared/footer'); ?>
